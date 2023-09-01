87fad65369e3bc5480821028543d98e91661a215
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-09-01 15:28:26 +0600

    add to test api

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
-import express, { Application, NextFunction, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
+import express, { Application, NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 // middleware
-app.use([cors(), express.json(), express.urlencoded({ extended: true })]);
+app.use([cors(), express.json(), express.urlencoded({ extended: true })])
+
+// Test api
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ success: true, message: 'Server start Success..' })
+})
 
 // not found route
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(300).json({
     success: false,
-    message: "Not Found",
-    errorMessages: [{ path: req.originalUrl, message: "API Not Found" }],
-  });
-  next();
-});
-export default app;
+    message: 'Not Found',
+    errorMessages: [{ path: req.originalUrl, message: 'API Not Found' }],
+  })
+  next()
+})
+export default app
