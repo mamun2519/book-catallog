4ed9a4bcb916beebde64a10191e86dda3f221a93
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-09-02 22:16:14 +0600

    add to enum from user route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,10 +1,16 @@
 import express from 'express'
 import { UseController } from './user.controller'
+import auth from '../../middleware/auth'
+import { ENUM_USER_ROLE } from '../../../enum/user'
 
 const router = express.Router()
-router.get('/:id', UseController.getByIdFromDB)
-router.patch('/:id', UseController.updateIntoDB)
-router.get('/:id', UseController.deleteByIdFromDB)
-router.get('/', UseController.getAllFromDB)
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UseController.getByIdFromDB)
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), UseController.updateIntoDB)
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  UseController.deleteByIdFromDB,
+)
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UseController.getAllFromDB)
 
 export const UserRoutes = router
