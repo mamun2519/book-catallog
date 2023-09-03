c674e7da5a232e66e0a311930f5d52e76786cc28
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-09-03 12:11:07 +0600

    add to change

--- dist/server.js
+++ dist/server.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const config_1 = require("./config");
+// database
 function bootstrap() {
     app_1.default.listen(config_1.env_config.port, () => console.log('Server Running port -', config_1.env_config.port));
 }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app'
 import { env_config } from './config'
 
+// database
 function bootstrap() {
   app.listen(env_config.port, () =>
     console.log('Server Running port -', env_config.port),
