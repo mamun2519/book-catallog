a7e59961de8d17b339dea3f36db3a451d5796566
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-09-03 11:34:24 +0600

    vercel dev

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,13 @@ app.use([cors(), express.json(), express.urlencoded({ extended: true })])
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ success: true, message: 'Server start Success..' })
 })
-//root route
+//root routes
 app.use('/api/v1', RootRoute)
 
 //add to global error handler middleware
 app.use(globalErrorHandler)
 
-// not found route
+// not found routes
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(300).json({
     success: false,
